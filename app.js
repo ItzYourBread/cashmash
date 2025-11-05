cbb9ff2d3b126f306eb6db48a95d272c36514c55
“ItzYourBread” <md9005876@gmail.com>
2025-11-06 02:22:36 +0600

    new domain added

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const cors = require('cors');
 app.use(cors({
   origin: [
     'https://cashmash.onrender.com',   // your hosted frontend (change this if different)
-    'https://www.cashmash.onrender.com',
+    'https://cashmash.watchnsfw.com',
     'http://localhost:3000',
     'http://127.0.0.1:5500', // <-- The origin of your HTML file (Live Server)
     'http://localhost:5500'
